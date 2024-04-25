01f14f6fe5a544b44a938e5b1a1aa79e2ea228ab
Indrajeety993648 <145670162+Indrajeety993648@users.noreply.github.com>
2024-04-25 23:22:32 +0530

    Update GameController.java

--- TicTacToe Backend/src/main/java/dev/indrajeet/tictactoe_backend/Controller/GameController.java
+++ TicTacToe Backend/src/main/java/dev/indrajeet/tictactoe_backend/Controller/GameController.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 public class GameController {
     public Game startGame(int dimension, List<Player> players) {
-        //TODO
-        //Validate if 2 players have the same symbol or not ?
-        //If 2 players have same symbol, throw some exception.
         return new Game(dimension, players);
     }
 
